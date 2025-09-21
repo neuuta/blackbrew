72fa07c0f6203f3176da22a3c7061e2ab27abdb5
neuuta <vetalzamkov@gmail.com>
2025-09-21 23:29:32 +0300

    Deploy static site to gh-pages

--- cart/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class GuestNameForm(forms.Form):
-    name = forms.CharField(
-        label='Ваше ім’я',
-        max_length=150,
-        widget=forms.TextInput(attrs={'placeholder': 'Введіть ваше ім’я'})
-    )
